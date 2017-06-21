ac32fb62708aa2e00510fc970ad182657463bdd4
Yi Zhao <zhaoyi0113@gmail.com>
2017-06-21 21:23:01 +1000

    update app path

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './'),
     publicPath: '/',
-    // filename: 'app.[hash].js'
+    //filename: 'app.[hash].js'
     filename: 'app.js'
   },
   module: {
