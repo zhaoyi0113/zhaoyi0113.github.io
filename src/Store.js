1d13a440449a929c046b2650a35e94caf550d4d1
Yi Zhao <zhaoyi0113@gmail.com>
2017-09-22 20:09:14 +1000

    add new blog

--- src/Store.js
+++ src/Store.js
@@ -56,6 +56,12 @@ export default class Store {
       description: 'When it comes to database design or analytic, query performance optimistic is always a hot topic. MongoDB provides a really great feather, which is “explain”, on helping DBAs to understand what’s going on inside the query command execution. After evaluating the explain, I have to say it is a really helpful and well-designed feather in terms of performance. In this article, I’d like discussing more in detailed about explain. It will help beginners on using it.',
       link: 'https://medium.com/@zhaoyi0113/how-to-use-explain-to-improvement-mongodb-performance-3adadb1228f1',
     },
+    {
+      id: 4,
+      title: 'Python implementation of batch gradient descent',
+      description: 'A brief introduction about python implementation of gradient descent',
+      link: 'https://medium.com/@zhaoyi0113/python-implementation-of-batch-gradient-descent-379fa19eb428'
+    }
   ]);
 
   @observable recentProjects = observable([{
