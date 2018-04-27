84abe2f0937978c7d6a7523eaf4b195ba182de18
Yi Zhao <zhaoyi0113@gmail.com>
2018-04-27 15:36:11 +1000

    update deploy gh pages

--- .circleci/deploy-ghpages.sh
+++ .circleci/deploy-ghpages.sh
@@ -4,25 +4,6 @@
 # Based on https://github.com/eldarlabs/ghpages-deploy-script/blob/master/scripts/deploy-ghpages.sh
 # Used with their MIT license https://github.com/eldarlabs/ghpages-deploy-script/blob/master/LICENSE
 
-# abort the script if there is a non-zero error
-set -e
-
-# show where we are on the machine
-pwd
-remote=$(git config remote.origin.url)
-
-# make a directory to put the gp-pages branch
-mkdir gh-pages-branch
-cd gh-pages-branch
-# now lets setup a new repo so we can update the gh-pages branch
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
-git init
-git remote add --fetch origin "$remote"
-
-
-# copy over or recompile the new site
-#cp -a "../${siteSource}/." .
 ls
 yarn install
 yarn run build
@@ -32,10 +13,8 @@ git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages
+git push --force --quiet origin master
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
-cd ..
-rm -rf gh-pages-branch
 
 echo "Finished Deployment!"
